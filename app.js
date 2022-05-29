524a8b2b151545d7991ce07feb4f5cb57866dc9f
Karan-Dayani <88270628+Karan-Dayani@users.noreply.github.com>
2022-05-29 20:58:26 +0530

    3rd Commit

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const playSection = document.querySelector(".play-section");
 const playBtn = document.querySelector(".play-btn");
+const levelDisplay = document.querySelector(".level-display");
+const gameArea = document.querySelector(".game-area");
 
 playBtn.onclick = () => {
     playSection.classList.add("fadeOut");
+    levelDisplay.classList.replace("fadeOut", "fadeIn");
+    setTimeout(() => {
+        levelDisplay.classList.replace("top-50", "top-0");
+        levelDisplay.classList.replace("translate-middle", "translate-middle-x");
+    }, 800);
+    gameArea.classList.replace("fadeOut", "fadeIn");
 }
