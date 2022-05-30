50d8441d5ee565259afda78968219df60a024f3c
Karan-Dayani <88270628+Karan-Dayani@users.noreply.github.com>
2022-05-30 19:20:43 +0530

    5th Commit

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ playBtn.onclick = () => {
         levelDisplay.classList.replace("top-50", "top-0");
         levelDisplay.classList.replace("translate-middle", "translate-middle-x");
     }, 800);
+    setTimeout(() => {
+        loop();
+    }, 5000);
     gameArea.classList.replace("fadeOut", "fadeIn");
 }
 
@@ -25,16 +28,18 @@ ctx.lineWidth = 3;
 const PADDLE_WIDTH = 100;
 const PADDLE_HEIGHT = 20;
 const PADDLE_MARGIN_BOTTOM = 50;
+const BALL_RADIUS = 10;
+let paddleX = (cvs.width - PADDLE_WIDTH)/2
 let leftArrow = false;
 let rightArrow = false;
 
 //! Creating the paddle
 const paddle = {
-    x : cvs.width/2 - PADDLE_WIDTH/2,
-    y : cvs.height - PADDLE_MARGIN_BOTTOM - PADDLE_HEIGHT,
-    width : PADDLE_WIDTH,
-    height : PADDLE_HEIGHT,
-    dx : 8
+    x: cvs.width / 2 - PADDLE_WIDTH / 2,
+    y: cvs.height - PADDLE_MARGIN_BOTTOM - PADDLE_HEIGHT,
+    width: PADDLE_WIDTH,
+    height: PADDLE_HEIGHT,
+    dx: 8
 }
 
 //! Drawing the paddle
@@ -47,20 +52,29 @@ function drawPaddle() {
 }
 
 //! Controling the paddle
-document.addEventListener("keydown", function(e) {
+document.addEventListener("keydown", function (e) {
     if (e.keyCode === 37) {
         leftArrow = true;
     } else if (e.keyCode === 39) {
         rightArrow = true;
     }
 });
-document.addEventListener("keyup", function(e) {
+document.addEventListener("keyup", function (e) {
     if (e.keyCode === 37) {
         leftArrow = false;
     } else if (e.keyCode === 39) {
         rightArrow = false;
     }
 });
+
+// document.addEventListener("mousemove", function(e) {
+//     var relativeX = e.clientX - cvs.left;
+//     if (relativeX > 0 && relativeX < cvs.width) {
+//         paddleX = relativeX - PADDLE_WIDTH / 2;
+//     }
+// });
+// && paddleX < cvs.width - PADDLE_WIDTH
+
 
 //! Moving the paddle
 function movePaddle() {
@@ -71,14 +85,81 @@ function movePaddle() {
     }
 }
 
+//! Creating the ball
+const ball = {
+    x: cvs.width / 2,
+    y: paddle.y - BALL_RADIUS,
+    radius: BALL_RADIUS,
+    speed: 4,
+    dx: 3 * (Math.random() * 2 - 1),
+    dy: -3
+}
+
+//! Drawing the ball
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
+    ctx.fillStyle = "#8FBDD3";
+    ctx.fill();
+    ctx.strokeStyle = "#A97155";
+    ctx.stroke()
+    ctx.closePath();
+}
+
+//! Moving the ball
+function moveBall() {
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+}
+
+//! Ball and wall collision detection
+function ballwallcollision() {
+    if (ball.x + ball.radius > cvs.width || ball.x - ball.radius < 0) {
+        ball.dx = - ball.dx;
+    }
+    if (ball.y - ball.radius < 0 ) {
+        ball.dy = - ball.dy
+    }
+    if (ball.y + ball.radius > cvs.height) {
+        resetBall();
+    }
+}
+
+//! Reseting the ball
+function resetBall() {
+    ball.x = cvs.width / 2;
+    ball.y = paddle.y - BALL_RADIUS;
+    ball.dx = 3 * (Math.random() * 2 - 1);
+    ball.dy = -3;
+}
+
+//! Ball and paddle collision detection
+function ballPaddleCollision() {
+    if (ball.x < paddle.x + paddle.width && ball.x > paddle.x && paddle.y < paddle.y + paddle.height && ball.y > paddle.y) {
+        //! check where the ball hit at the paddle
+        let collidePoint = ball.x - (paddle.x + paddle.width / 2);
+        //! normalise the values
+        collidePoint = collidePoint / (paddle.width / 2);
+        //! calculate the angle of the ball
+        let angle = collidePoint * Math.PI / 3;
+        //! changing direction
+        ball.dx = ball.speed * Math.sin(angle);
+        ball.dy = -ball.speed * Math.cos(angle);
+    }
+}
+
 //! Draw function
 function draw() {
     drawPaddle();
+    drawBall();
 }
 
 //! Update function
 function update() {
     movePaddle();
+    moveBall();
+    ballwallcollision();
+    ballPaddleCollision();
 }
 
 //! Game loop
@@ -88,4 +169,3 @@ function loop() {
     update();
     requestAnimationFrame(loop);
 }
-loop();
